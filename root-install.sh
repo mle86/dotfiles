8645feb568f947cb53d0a31cd3c1898896a128c1
mle <maximilian@eul.cc>
2016-06-10 22:17:27 +0200

    root-install: install my clerr, gl, walk programs

--- root-install.sh
+++ root-install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 . $(dirname "$0")/shfn.sh
+. $(dirname "$0")/githubfn.sh
 
 if [ "`id -u`" != 0 ]; then
 	warn "You are not root!"
@@ -11,6 +12,10 @@ bindest=/usr/local/bin
 [ -d "$bindest" ] || mkdir -p -- "$bindest"
 
 ################################################################################
+
+ask_github "mle86/clerr"
+ask_github "mle86/gl"
+ask_github "mle86/walk"
 
 for file in $(binfiles); do
 	ask_copy "$bindest/$file" "bin/$file"
